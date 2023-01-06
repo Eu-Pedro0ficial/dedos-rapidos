e9d4b2580710dbe8a22a27d2125e4cd0db0fb160
Pedro Cardoso da Silva <pcsilvamaster@gmail.com>
2023-01-06 19:39:15 -0300

    preview modifications

--- src/js/functions/helpers.js
+++ src/js/functions/helpers.js
@@ -1,4 +1,4 @@
-import {letras, data, container, content, lifeInGame} from './variaveis.js';
+import {letras, data, container, content, lifeInGame, score_container, hit_area, titleLoser, titleStart, loser, start} from './variaveis.js';
 import { checkLimitInGreenArea } from './validate.js';
 import {createElement, addElementInPage} from './element.js'
 import animation from './animation.js';
@@ -24,9 +24,11 @@ function clearIntervalAll(array){
 let setintervalIndex = [];
 
 function startGame(){
+    appearElement([hit_area, score_container, lifeInGame, container]);
     setintervalIndex.push(listeningScore());
     setintervalIndex.push(listeningHTML());
     setintervalIndex.push(checkLimitInGreenArea());
+    disappearElement([titleStart, start]);
 
 }
 
@@ -37,8 +39,22 @@ function restartToGame(){
     }
     lifeInGame.dataset.life = 3;
     lifeInGame.textContent = lifeInGame.dataset.life = 3;
+    disappearElement([titleLoser, loser])
     startGame();
 }
+
+function appearElement(array){
+    array.forEach(element => {
+        element.style.opacity = "1";
+    });
+
+}
+function disappearElement(array){
+    array.forEach(element => {
+        element.style.opacity = "0";
+    });
+}
+
 
 function listeningHTML(){
     return setInterval(() => {
@@ -54,8 +70,15 @@ function listeningScore(){
     return setInterval(()=>{
         if(lifeInGame.dataset.life <= 0){
             clearIntervalAll(setintervalIndex);
+            appearElement([titleLoser, loser])
         }
     }, 400)
 }
+
+
+/*
+    display: none; //some com tudo, até o espaço
+    opacity: 0; // trasparencia
+*/
 
 export {deleteCharacter, addStyleFromLoser, clearIntervalAll, listeningHTML, listeningScore, startGame, restartToGame};
--- src/js/functions/variaveis.js
+++ src/js/functions/variaveis.js
@@ -4,12 +4,14 @@ const score_container = document.getElementById("score-container");
 const lifeInGame = document.getElementById("life-in-game");
 const content = document.getElementById("content");
 const letras = document.getElementsByClassName("character")
+const titleStart = document.getElementById("titleStart");
 const start = document.getElementById("startGame");
+const titleLoser = document.getElementById("titleLoser");
 const loser = document.getElementById("loser");
 const heightContainer = container.offsetHeight;
 
 const res = await fetch("../../settings.json");
 const data = await res.json()
 
-export {container, hit_area, score_container, lifeInGame, content, heightContainer, letras, data, start, loser};
+export {titleLoser, titleStart, container, hit_area, score_container, lifeInGame, content, heightContainer, letras, data, start, loser};
 
